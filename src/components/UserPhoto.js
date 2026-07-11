2e4a7245e8cc5f3e3ef08f8f835281c7751bf2c3
FlakoBB <bbjosephryan@gmail.com>
2026-07-11 17:25:20 -0600

    fix: user photo

--- src/components/UserPhoto.js
+++ src/components/UserPhoto.js
@@ -1,8 +1,10 @@
+import photo from '../assets/images/user-photo.jpg'
+
 export const UserPhoto = () => {
   return `
     <figure class="user-photo">
       <img
-        src='/user-photo.jpg'
+        src='${photo}'
         draggable='false'
       />
     </figure>
